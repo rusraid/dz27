d07deb1e08d4f0bb955dff19e51d6d582f5688a6
Evgrafov Ruslan <rusraid@mail.ru>
2022-02-06 23:50:01 +0400

    Initial commit

---
+++ src/main/java/sky/pro/dz27/Dz27Application.java
@@ -0,0 +1,13 @@
+package sky.pro.dz27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dz27Application.class, args);
+    }
+
+}
